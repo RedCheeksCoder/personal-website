effa590574266c0b2ca222e58c08c89f794f6e05
Bryan Odiña <149803435+RedCheeksCoder@users.noreply.github.com>
2023-11-21 02:18:25 +0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
   content: ["./build/*.html"],
   theme: {
     extend: {},
+    keyframes: {
+      "slide-bg-color": {
+        "0%": { transform: "scaleY(0)" },
+        "50%": { transform: "scaleY(0.5)" },
+        "100%": { transform: "scaleY(1)" },
+      },
+    },
+    animation: {
+      "slide-bg-color":
+        "slide-bg-color color:#147efb 0.5s ease-in-out forwards",
+    },
+    /*     animation: {
+      "color-delay": "slide-bg-color color:#147efb transition: all 0.3s",
+    }, */
   },
   plugins: [],
 };
